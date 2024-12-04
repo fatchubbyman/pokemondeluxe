636152e2f5f8cc1e523051a293adb79d6df4d9db
chubby <jatintheboy@gmail.com>
2024-12-04 15:12:17 +0530

    Add files via upload

--- pokemondeluxe.py
+++ pokemondeluxe.py
@@ -1,59 +1,58 @@
-#                                                                  ideas/scrapped ideas
-#make all the areas of the pokemon game we wanna have in the game
-#webscrape from https://pokemondb.net/ and get all the data for all the stats of the pokemon
-#learn how to use classes and check if we can make more methods(functions) to use in game like the items ability
-#this time expand to more regions(maybe contain 4-5 regions in this game!?)
-#we are learning to use objects classes web scraping and using a variety of functions to make this game
-# get a way to get out an input prompt everytime and ask what to do, we will have the most amount 0f functions ever
-# a battle mode, a more normal prompt mode asking you what to do, wild pokemon fight mode, buying mode, starting mode to select pokemon
+from bs4 import BeautifulSoup
+import requests
 
-
-import pandas as pd
-
-df = pd.read_csv('Pokemon.csv')
+pokemon_logged = []
 class Pokemon:
-    def __init__(self,sp_attack: int,sp_defense: int,hp: int,speed : int,name : str,attack : int ,defense : int,exp:int,type1 :str,type2:str,total: int):
-        self.name = name
-        self.sp_attack = sp_attack
-        self.sp_defense = sp_defense
+    def __init__(self, hp, moves: dict,weakness,type1,type2 = None, level = 5, defense ,spatk,spdef,speed,evolution:dict,name,attack):
+
         self.hp = hp
-        self.speed = speed
+        self.type1 = type1
+        self.weakness = weakness
+        self.moves = moves
+        self.level = level
+        self.evolution = evolution
         self.attack = attack
         self.defense = defense
-        self.exp = exp
-        self.type1 = type1
-        self.type2 = type2
-        self.total = total
-        
-    def Protein(self):
-        self.hp += 10
-        print("Your %s has a new base HP of %d" % (self.name,self.attack))
+        self.spatk = spatk
+        self.spdef = spdef
+        self.speed = speed
+        self.name = name
+        self
+
+    #need a repr like function to check if the pokemon needs to evolve or not according to level
+
+def evolution_log_error(poki,pokemon_logged = pokemon_logged):
+    span_tag = poki.find('span', class_ = 'infocard-lg-data text-muted')
+    name = span_tag.find('a', class_ = 'ent-name').text.strip()
+    if name in pokemon_logged:
+        return True
+    return False
+
+
+games = {'kanto':'firered-leafgreen','unova':'black-white','sinnoh':'platinum','hoenn':'ruby-sapphire-emerald','alola':'sun-moon','kalos':'x-y','johto':'heartgold-soulsilver','galar':'sword-shield'}
+game_choice = input('Which game would you like to play? (kanto,johto,hoenn,sinnoh,unova,kalos,alola,galar) ')
+
+url = 'https://pokemondb.net/pokedex/game/' + games[game_choice.lower()]
+rqsts = requests.get(url)
+soup = BeautifulSoup(rqsts.content,'lxml')
+pokemon_stats_filler = soup.find('div' , class_ = 'infocard-list infocard-list-pkmn-lg')
+pokemon_stats_filler = pokemon_stats_filler.find_all('div', class_ = 'infocard ')
+for poki in pokemon_stats_filler:
+    if evolution_log_error(poki= poki) is True:
+        continue
+    span_tag = poki.find('span', class_ = 'infocard-lg-data text-muted')
+    name = span_tag.find('a', class_ = 'ent-name').text.strip()
+    href = name.get('href')
+    name = name.text.strip()
+    link = 'https://pokemondb.net/' + href
+    rqsts = requests.get(link)
+    rsoup = BeautifulSoup(rqsts.content,'lxml')
     
-    def Calcium(self):
-        self.sp_attack += 10
-        print("Your %s has a new base Sp.Attack of %d" % (self.name,self.sp_attack))
-    
-    def HP_Up(self):
-        self.hp += 10
-        print("Your %s has a new base HP of %d" % (self.name,self.hp))
-    
-    def Carbos(self):
-        self.speed += 10
-        print("Your %s has a new base speed of %d" % (self.name,self.speed))
-        
-    def Zinc(self):
-        self.sp_defense += 10
-        print("Your %s has a new base SP.Defense of %d" % (self.name,self.sp_defense))
-    
-    def Iron(self):
-        self.defense += 10
-        print("Your %s has a new base defense of %d" % (self.name,self.defense))
-    
-class MyPokemon(Pokemon):
-    def __init__(self, sp_attack: int, sp_defense: int, hp: int, speed: int, name: str, attack: int, defense: int, exp: int, type1: str, type2: str, total: int):
-        super().__init__(sp_attack, sp_defense, hp, speed, name, attack, defense, exp, type1, type2, total)
-    
-        
+
+
+
+
+
         
 
 
