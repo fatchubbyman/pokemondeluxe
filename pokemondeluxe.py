9ce6a04f099ca711d59edf20c6b8b647005417fc
chubby <jatintheboy@gmail.com>
2024-12-04 15:40:04 +0530

    Add files via upload

--- pokemondeluxe.py
+++ pokemondeluxe.py
@@ -3,21 +3,22 @@ import requests
 
 pokemon_logged = []
 class Pokemon:
-    def __init__(self, hp, moves: dict,weakness,type1,type2 = None, level = 5, defense ,spatk,spdef,speed,evolution:dict,name,attack):
+    def __init__(self, hp: int, moves: dict,weakness:list,type1:str, spatk: int ,spdef: int,speed: int,evolution:dict,name: str,attack: int,defense: int,evolution_level,type2 = None, level = 5,moveset={}):
 
         self.hp = hp
         self.type1 = type1
+        self.type2 = type2
         self.weakness = weakness
-        self.moves = moves
+        self.moveset = moveset
         self.level = level
         self.evolution = evolution
+        self.evolution_level = evolution_level
         self.attack = attack
         self.defense = defense
         self.spatk = spatk
         self.spdef = spdef
         self.speed = speed
         self.name = name
-        self
 
     #need a repr like function to check if the pokemon needs to evolve or not according to level
 
@@ -28,6 +29,13 @@ def evolution_log_error(poki,pokemon_logged = pokemon_logged):
         return True
     return False
 
+def weakness_logger(type1,type2 = None):
+    pass
+
+def evolution_line_logger():
+    pass
+
+def evolving_checker()
 
 games = {'kanto':'firered-leafgreen','unova':'black-white','sinnoh':'platinum','hoenn':'ruby-sapphire-emerald','alola':'sun-moon','kalos':'x-y','johto':'heartgold-soulsilver','galar':'sword-shield'}
 game_choice = input('Which game would you like to play? (kanto,johto,hoenn,sinnoh,unova,kalos,alola,galar) ')
@@ -47,7 +55,10 @@ for poki in pokemon_stats_filler:
     link = 'https://pokemondb.net/' + href
     rqsts = requests.get(link)
     rsoup = BeautifulSoup(rqsts.content,'lxml')
-    
+    types = rsoup.find_all()
+
+
+
 
 
 
